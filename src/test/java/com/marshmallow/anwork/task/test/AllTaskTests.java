da3769549fd312218ad7e316a37b166626f2edb1
Andrew Keesler <andrew.keesler@silabs.com>
2017-09-16 14:52:03 -0400

    Delete the LoggingTaskManager. We are not going to use this guy.

--- src/test/java/com/marshmallow/anwork/task/test/AllTaskTests.java
+++ src/test/java/com/marshmallow/anwork/task/test/AllTaskTests.java
@@ -6,7 +6,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    LoggingTaskManagerTest.class,
     TaskManagerSerializerTest.class,
     TaskManagerTest.class,
     TaskSerializerTest.class,
