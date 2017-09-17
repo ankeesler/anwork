d361d575e5d80bda02f8f97e3972fcb8f501a0fa
Andrew Keesler <andrew.keesler@silabs.com>
2017-09-17 14:14:16 -0400

    Add a couple more task manager serialization tests.

--- src/test/java/com/marshmallow/anwork/task/test/TaskManagerSerializerTest.java
+++ src/test/java/com/marshmallow/anwork/task/test/TaskManagerSerializerTest.java
@@ -21,6 +21,40 @@ import org.junit.Test;
  */
 public class TaskManagerSerializerTest extends SerializerTest<TaskManager> {
 
+  private static class TaskInfo {
+    private final String name;
+    private final String description;
+    private final int priority;
+
+    public TaskInfo(String name, String description, int priority) {
+      this.name = name;
+      this.description = description;
+      this.priority = priority;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+
+    public int getPriority() {
+      return priority;
+    }
+  }
+
+  private static void assertTaskManagerEquals(TaskManager manager, TaskInfo...infos) {
+    Task[] tasks = manager.getTasks();
+    assertEquals(infos.length, tasks.length);
+    for (int i = 0; i < infos.length; i++) {
+      assertEquals(infos[i].getName(), tasks[i].getName());
+      assertEquals(infos[i].getDescription(), tasks[i].getDescription());
+      assertEquals(infos[i].getPriority(), tasks[i].getPriority());
+    }
+  }
+
   private TaskManager manager = new TaskManager();
 
   /**
@@ -33,7 +67,7 @@ public class TaskManagerSerializerTest extends SerializerTest<TaskManager> {
   @Test
   public void testNoTasks() throws IOException {
     manager = runSerialization(manager);
-    assertEquals(0, manager.getTasks().length);
+    assertTaskManagerEquals(manager);
   }
 
   @Test
@@ -41,11 +75,9 @@ public class TaskManagerSerializerTest extends SerializerTest<TaskManager> {
     manager.createTask("task-a", "This is task a", 1);
     manager = runSerialization(manager);
 
-    Task[] tasks = manager.getTasks();
-    assertEquals(1, tasks.length);
-    assertEquals("task-a", tasks[0].getName());
-    assertEquals("This is task a", tasks[0].getDescription());
-    assertEquals(1, tasks[0].getPriority());
+    assertTaskManagerEquals(manager, new TaskInfo[] {
+        new TaskInfo("task-a", "This is task a", 1),
+    });
   }
 
   @Test
@@ -55,19 +87,76 @@ public class TaskManagerSerializerTest extends SerializerTest<TaskManager> {
     manager.createTask("task-c", "This is task c", 0);
     manager = runSerialization(manager);
 
+    assertTaskManagerEquals(manager, new TaskInfo[] {
+        new TaskInfo("task-c", "This is task c", 0),
+        new TaskInfo("task-a", "This is task a", 1),
+        new TaskInfo("task-b", "This is task b", 2),
+    });
+  }
+
+  @Test
+  public void testTasksWithSpaces() throws IOException {
+    manager.createTask("task a", "this is task a", 1);
+    manager.createTask("t a s k   b", "this is task b", 2);
+    manager = runSerialization(manager);
+
     Task[] tasks = manager.getTasks();
-    assertEquals(3, tasks.length);
+    assertEquals(2, tasks.length);
 
-    assertEquals("task-c", tasks[0].getName());
-    assertEquals("This is task c", tasks[0].getDescription());
-    assertEquals(0, tasks[0].getPriority());
+    assertEquals("task a", tasks[0].getName());
+    assertEquals("this is task a", tasks[0].getDescription());
+    assertEquals(1, tasks[0].getPriority());
 
-    assertEquals("task-a", tasks[1].getName());
-    assertEquals("This is task a", tasks[1].getDescription());
-    assertEquals(1, tasks[1].getPriority());
+    assertEquals("t a s k   b", tasks[1].getName());
+    assertEquals("this is task b", tasks[1].getDescription());
+    assertEquals(2, tasks[1].getPriority());
 
-    assertEquals("task-b", tasks[2].getName());
-    assertEquals("This is task b", tasks[2].getDescription());
-    assertEquals(2, tasks[2].getPriority());
+    assertTaskManagerEquals(manager, new TaskInfo[] {
+        new TaskInfo("task a", "this is task a", 1),
+        new TaskInfo("t a s k   b", "this is task b", 2),
+    });
+  }
+
+  @Test
+  public void testALotOfTasks() throws IOException {
+    for (int i = 0; i < 100; i++) {
+      manager.createTask("task-" + i, "this is task " + i, i);
+    }
+
+    manager = runSerialization(manager);
+
+    Task[] tasks = manager.getTasks();
+    assertEquals(100, tasks.length);
+    for (int i = 0; i < 100; i++) {
+      assertEquals("task-" + i, tasks[i].getName());
+      assertEquals("this is task " + i, tasks[i].getDescription());
+      assertEquals(i, tasks[i].getPriority());
+    }
+  }
+
+  @Test
+  public void testLargeTasks() throws IOException {
+    String message
+        = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed metus non nunc"
+          + " varius porttitor. Vivamus ut bibendum eros, nec rutrum nulla. Vestibulum suscipita"
+          + " dolor quis auctor. Vestibulum sodales quis velit ut mollis. Phasellus mattis tempor"
+          + " arcu et efficitur. Donec ac elit efficitur, facilisis ligula et, tristique sapien."
+          + " Mauris feugiat ante sed accumsan sollicitudin. Aliquam laoreet eros urna, nec"
+          + " efficitur metus aliquam facilisis. Ut interdum nec dolor id dictum. Nam fringilla"
+          + " pulvinar ex placerat scelerisque. Nullam vestibulum mi eget risus malesuada volutpat."
+          + " Mauris pulvinar risus et faucibus faucibus. Nulla facilisi. Duis non nunc nibh."
+          + " Integer orci odio, blandit cursus ornare a, molestie ut massa. Integer ultricies"
+          + " rutrum enim, euismod posuere mauris luctus vel.";
+    manager.createTask("this is task 1", message, 1);
+    manager.createTask("this is task 2", message, 2);
+    manager.createTask("this is task 3", message, 3);
+
+    manager = runSerialization(manager);
+
+    assertTaskManagerEquals(manager, new TaskInfo[] {
+        new TaskInfo("this is task 1", message, 1),
+        new TaskInfo("this is task 2", message, 2),
+        new TaskInfo("this is task 3", message, 3),
+    });
   }
 }
