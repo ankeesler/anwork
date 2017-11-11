c9df006ccd7ca91b4943078af480fb9a1c032cf0
Andrew Keesler <andrew.keesler@silabs.com>
2017-11-10 19:02:48 -0500

    Try out a new format for GitHub markdown documentation.

--- doc/CLI.md
+++ doc/CLI.md
@@ -1,32 +1,33 @@
 This documentation is generated from com.marshmallow.anwork.app.cli.GithubReadmeDocumentationGenerator
 
-#  *anwork*: ANWORK CLI commands
-## Flags
-- -c|--context (STRING name: The name of the persistence context): Set the persistence context
-- -d|--debug: Turn on debug printing
-- -n|--no-persist: Do not persist any task information
-- -o|--output (STRING directory: The directory at which to output the persistence data): Set persistence output directory
-## Commands
-- anwork *summary* (NUMBER days: Number of days to look back for work): Show a summary of the past days of work
-# anwork *journal*: Journal commands...
-## Commands
-- anwork journal *show* (STRING task-name: The name of the task whose journal entries will be shown): Show the entries in the journal for a task
-- anwork journal *show-all*: Show all of the entries in the journal
-# anwork *task*: Task commands...
-## Flags
-- -e|--description (STRING description: The description of the task): The description of the task
-- -p|--priority (NUMBER priority: The priority of the task): The priority of the task
-## Commands
-- anwork task *create* (STRING task-name: The name of the task to create): Create a task
-- anwork task *delete* (STRING task-name: The name of the task to delete): Delete a task
-- anwork task *delete-all*: Delete all tasks
-- anwork task *note* (STRING task-name: The name of the task to which to add a note) (STRING note: The note to add to a task): Add a note to a task
-- anwork task *set-blocked* (STRING task-name: The name of the task to set as blocked): Set a task as blocked
-- anwork task *set-finished* (STRING task-name: The name of the task to set as finished): Set a task as finished
-- anwork task *set-priority* (STRING task-name: The name of the task to set as finished) (NUMBER priority: The priority to set on the task): Set the priority of a task
-- anwork task *set-running* (STRING task-name: The name of the task to set as running): Set a task as running
-- anwork task *set-waiting* (STRING task-name: The name of the task to set as waiting): Set a task as waiting
-## Flags
-- -s|--short: Show a shorter description of all of the tasks
-## Commands
-- anwork task *show*: Show all tasks
+`anwork [-d|--debug] [-c|--context <name:STRING>] [-o|--output <directory:STRING>] [-n|--no-persist]` ... : ANWORK CLI commands
+* `anwork summary <days:NUMBER>`
+* * Show a summary of the past days of work
+
+`anwork journal` ... : Journal commands...
+* `anwork journal show <task-name:STRING>`
+* * Show the entries in the journal for a task
+* `anwork journal show-all`
+* * Show all of the entries in the journal
+
+`anwork journal task` ... : Task commands...
+* `anwork journal task create [-e|--description <description:STRING>] [-p|--priority <priority:NUMBER>] <task-name:STRING>`
+* * Create a task
+* `anwork journal task delete <task-name:STRING>`
+* * Delete a task
+* `anwork journal task delete-all`
+* * Delete all tasks
+* `anwork journal task note <task-name:STRING> <note:STRING>`
+* * Add a note to a task
+* `anwork journal task set-blocked <task-name:STRING>`
+* * Set a task as blocked
+* `anwork journal task set-finished <task-name:STRING>`
+* * Set a task as finished
+* `anwork journal task set-priority <task-name:STRING> <priority:NUMBER>`
+* * Set the priority of a task
+* `anwork journal task set-running <task-name:STRING>`
+* * Set a task as running
+* `anwork journal task set-waiting <task-name:STRING>`
+* * Set a task as waiting
+* `anwork journal task show [-s|--short]`
+* * Show all tasks
--- src/main/java/com/marshmallow/anwork/app/cli/GithubReadmeDocumentationGenerator.java
+++ src/main/java/com/marshmallow/anwork/app/cli/GithubReadmeDocumentationGenerator.java
@@ -9,6 +9,30 @@ import java.util.stream.Collectors;
  * Github README format.
  *
  * <p>
+ * The format of this documentation is roughly the following.
+ * <pre>
+ * `root-list [-d|--debug] ...` : root-list description
+ * {@literal *} `root-list command-a {@literal <}foo:STRING{@literal >} {@literal <}bar:NUMB...`
+ *
+ * `root-list sub-list-a ...`: sub-list-a description
+ * {@literal *} `root-list sub-list-a command-a {@literal <}foo:STRING{@literal >}`
+ * {@literal *} {@literal *} command-a description
+ * {@literal *} `root-list sub-list-a command-b [-a--andrew {@literal <}foo:STRING{@literal >}]`
+ * {@literal *} {@literal *} command-b description
+ * {@literal *} `root-list sub-list-a command-c {@literal <}foo:STRING{@literal >} {@literal <}...`
+ * {@literal *} {@literal *} command-c description
+ *
+ * `root-list sub-list-b [-C {@literal <}directory:STRING{@literal >}] ...`: sub-list-b description
+ * {@literal *} `root-list sub-list-b command-a {@literal <}foo:STRING{@literal >}`
+ * {@literal *} {@literal *} command-a description
+ * {@literal *} `root-list sub-list-b command-b`
+ * {@literal *} {@literal *} command-b description
+ * {@literal *} `root-list sub-list-b command-c {@literal <}foo:STRING{@literal >} {@literal <}...`
+ * {@literal *} {@literal *} command-c description
+ * </pre>
+ * </p>
+ *
+ * <p>
  * Created Oct 16, 2017
  * </p>
  *
@@ -16,18 +40,7 @@ import java.util.stream.Collectors;
  */
 class GithubReadmeDocumentationGenerator implements DocumentationGenerator, Visitor {
 
-  private static final String NO_DESCRIPTION = "<no description>";
-
-  private static enum State {
-    NONE,
-    FLAG,
-    LIST,
-    COMMAND,
-    ;
-  }
-
   private PrintWriter writer;
-  private State state;
   private Stack<String> listStack;
 
   @Override
@@ -38,97 +51,90 @@ class GithubReadmeDocumentationGenerator implements DocumentationGenerator, Visi
   @Override
   public void generate(Cli cli, PrintWriter writer) {
     this.writer = writer;
-    this.state = State.NONE;
     this.listStack = new Stack<String>();
 
     writer.println("This documentation is generated from " + getClass().getName());
-    writer.println();
     cli.visit(this);
-  }
-
-  private void checkFlagState() {
-    if (state != State.FLAG) {
-      writer.println("## Flags");
-      state = State.FLAG;
-    }
-  }
-
-  private void checkListState() {
-    if (state != State.LIST) {
-      state = State.LIST;
-    }
-  }
-
-  private void checkCommandState() {
-    if (state != State.COMMAND) {
-      writer.println("## Commands");
-      state = State.COMMAND;
-    }
   }
 
   @Override
   public void visitFlag(Flag flag) {
-    checkFlagState();
-    StringBuilder lineBuilder = new StringBuilder();
-    lineBuilder.append("- -").append(flag.getShortFlag());
-    if (flag.hasLongFlag()) {
-      lineBuilder.append("|--" + flag.getLongFlag());
-    }
-    if (flag.hasArgument()) {
-      lineBuilder.append(' ').append(makeArgumentText(flag.getArgument()));
-    }
-    lineBuilder.append(": ").append((flag.hasDescription()
-                                     ? flag.getDescription()
-                                     : NO_DESCRIPTION));
-    writer.println(lineBuilder.toString());
   }
 
   @Override
   public void visitList(List list) {
-    checkListState();
-    String prefix = listStack.stream().collect(Collectors.joining(" "));
-    writer.println(String.format("# %s *%s*: %s",
-                                 prefix,
-                                 list.getName(),
-                                 (list.hasDescription()
-                                  ? list.getDescription()
-                                  : NO_DESCRIPTION)));
+    writer.println();
+
+    StringBuilder lineBuilder = new StringBuilder();
+    lineBuilder.append('`');
+    lineBuilder.append(makeListPrefix());
+    lineBuilder.append(list.getName());
+    lineBuilder.append(makeFlagText(list.getFlags()));
+    lineBuilder.append('`');
+    lineBuilder.append(" ...");
+    if (list.hasDescription()) {
+      lineBuilder.append(" : ");
+      lineBuilder.append(list.getDescription());
+    }
+    writer.println(lineBuilder.toString());
     listStack.push(list.getName());
   }
 
   @Override
   public void leaveList(List list) {
-    checkListState();
-    listStack.pop();
   }
 
   @Override
   public void visitCommand(Command command) {
-    checkCommandState();
-    String prefix = listStack.stream().collect(Collectors.joining(" "));
-    writer.print(String.format("- %s *%s*",
-                                 prefix,
-                                 command.getName()));
+    StringBuilder lineBuilder = new StringBuilder();
+    lineBuilder.append("* ");
+    lineBuilder.append('`');
+    lineBuilder.append(makeListPrefix());
+    lineBuilder.append(command.getName());
+    lineBuilder.append(makeFlagText(command.getFlags()));
     for (Argument argument : command.getArguments()) {
-      writer.print(" " + makeArgumentText(argument));
+      lineBuilder.append(makeArgumentText(argument));
     }
-    writer.println(String.format(": %s",
-                                 (command.hasDescription()
-                                  ? command.getDescription()
-                                  : NO_DESCRIPTION)));
+    lineBuilder.append('`');
+    writer.println(lineBuilder.toString());
+
+    if (command.hasDescription()) {
+      writer.println("* * " + command.getDescription());
+    }
+  }
+
+  private String makeListPrefix() {
+    return (listStack.stream().collect(Collectors.joining(" "))
+            + (listStack.empty() ? "" : " "));
+  }
+
+  private String makeFlagText(Flag[] flags) {
+    StringBuilder textBuilder = new StringBuilder();
+    for (Flag flag : flags) {
+      textBuilder.append(' ');
+      textBuilder.append('[');
+      textBuilder.append(Flag.FLAG_START).append(flag.getShortFlag());
+      if (flag.hasLongFlag()) {
+        textBuilder.append('|')
+                   .append(Flag.FLAG_START)
+                   .append(Flag.FLAG_START)
+                   .append(flag.getLongFlag());
+      }
+      if (flag.hasArgument()) {
+        textBuilder.append(makeArgumentText(flag.getArgument()));
+      }
+      textBuilder.append(']');
+    }
+    return textBuilder.toString();
   }
 
   private static String makeArgumentText(Argument argument) {
-    StringBuilder builder = new StringBuilder();
-    builder.append("(")
-           .append(argument.getType().getName())
-           .append(" ")
-           .append(argument.getName());
-    if (argument.hasDescription()) {
-      builder.append(": ")
-             .append(argument.getDescription());
-    }
-    builder.append(')');
-    return builder.toString();
+    return new StringBuilder().append(' ')
+                              .append('<')
+                              .append(argument.getName())
+                              .append(':')
+                              .append(argument.getType().getName())
+                              .append('>')
+                              .toString();
   }
 }
