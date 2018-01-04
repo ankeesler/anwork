3f0dea17ee2f99b4b64406c628b015e09c3f99cd
Andrew Keesler <ankeesler1@gmail.com>
2018-01-03 23:45:42 -0500

    More reorganization of commands.

--- cmd/anwork/anwork.go
+++ cmd/anwork/anwork.go
@@ -3,11 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 
 	"github.com/ankeesler/anwork/storage"
 	"github.com/ankeesler/anwork/task"
 )
+
+// TODO: rename me to main.go!
 
 // This is the version of this anwork app.
 const version = 1
@@ -17,22 +18,6 @@ var (
 	help, debug   bool
 	context, root string
 )
-
-// This map stores the functions associated with each of the command line actions.
-var commands = []command{
-	command{name: "version", usage: "(no arguments)", action: versionAction},
-	command{name: "create", usage: "<task-name>", action: createAction},
-	command{name: "delete", usage: "<task-name>", action: deleteAction},
-	command{name: "show", usage: "(no arguments)", action: showAction},
-	command{name: "note", usage: "<task-name> <note>", action: noteAction},
-	command{name: "set-running", usage: "<task-name>", action: setStateAction},
-	command{name: "set-blocked", usage: "<task-name>", action: setStateAction},
-	command{name: "set-waiting", usage: "<task-name>", action: setStateAction},
-	command{name: "set-finished", usage: "<task-name>", action: setStateAction},
-	command{name: "journal", usage: "(no arguments)", action: journalAction},
-}
-
-var shiftIndex int = 0
 
 func dbgfln(format string, stuff ...interface{}) {
 	if debug {
@@ -56,95 +41,6 @@ func writeManager(persister *storage.Persister, context string, manager *task.Ma
 	}
 }
 
-// This function gets the next command line argument (via flag.Args), or panics if there is no such
-// argument.
-func shift() string {
-	if shiftIndex >= flag.NArg() {
-		msg := fmt.Sprintf("Failed to retrieve arg %d", shiftIndex)
-		panic(msg)
-	}
-	val := flag.Arg(shiftIndex)
-	shiftIndex++
-	return val
-}
-
-func versionAction(command string, manager *task.Manager) bool {
-	fmt.Println("ANWORK Version =", version)
-	return false
-}
-
-func createAction(command string, manager *task.Manager) bool {
-	dbgfln("Creating task...")
-	name := shift()
-	manager.Create(name)
-	return true
-}
-
-func showAction(command string, manager *task.Manager) bool {
-	printer := func(state task.State) {
-		fmt.Printf("%s tasks:\n", strings.ToUpper(task.StateNames[state]))
-		for _, task := range manager.Tasks() {
-			if task.State() == state {
-				fmt.Printf("  %s (%d)\n", task.Name(), task.ID())
-			}
-		}
-	}
-	printer(task.StateRunning)
-	printer(task.StateBlocked)
-	printer(task.StateWaiting)
-	printer(task.StateFinished)
-	return false
-}
-
-func noteAction(command string, manager *task.Manager) bool {
-	name := shift()
-	note := shift()
-	manager.Note(name, note)
-	return true
-}
-
-func deleteAction(command string, manager *task.Manager) bool {
-	dbgfln("Deleting task...")
-	name := shift()
-	if !manager.Delete(name) {
-		fmt.Printf("Error! Unknown task: %s\n", name)
-		return false
-	} else {
-		return true
-	}
-}
-
-func setStateAction(command string, manager *task.Manager) bool {
-	name := shift()
-
-	var state task.State
-	command = strings.TrimPrefix(command, "set-")
-	switch command {
-	case "running":
-		state = task.StateRunning
-	case "blocked":
-		state = task.StateBlocked
-	case "waiting":
-		state = task.StateWaiting
-	case "finished":
-		state = task.StateFinished
-	default:
-		panic("Unknown state: " + command)
-	}
-	manager.SetState(name, state)
-	return true
-}
-
-func journalAction(command string, manager *task.Manager) bool {
-	es := manager.Journal().Events
-	for i := len(es) - 1; i >= 0; i-- {
-		e := es[i]
-		fmt.Printf("[%s %s %d %02d:%02d]: %s\n", e.Date.Weekday(), e.Date.Month(), e.Date.Day(),
-			e.Date.Hour(), e.Date.Minute(), e.Title)
-	}
-	return false
-}
-
 func findAction(name string) func(string, *task.Manager) bool {
 	for _, c := range commands {
 		if c.name == name {
@@ -163,6 +59,7 @@ func main() {
 
 	flag.Parse()
 	if help {
+		// TODO: write our own usage with the commands!
 		flag.Usage()
 		return
 	}
@@ -172,6 +69,7 @@ func main() {
 		flag.Usage()
 		return
 	}
+	firstArg := flag.Arg(0)
 
 	persister := storage.Persister{root}
 	manager := task.NewManager()
@@ -184,7 +82,6 @@ func main() {
 	}
 	dbgfln("Manager is %s", manager)
 
-	firstArg := shift()
 	action := findAction(firstArg)
 	if action == nil {
 		fmt.Println("Error! Unknown command line argument:", firstArg)
--- cmd/anwork/command.go
+++ cmd/anwork/command.go
@@ -1,12 +1,179 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"strings"
+
 	"github.com/ankeesler/anwork/task"
 )
 
 // A command is a keyword (see name field) passed to the anwork executable that provokes some
 // functionality (see action field).
 type command struct {
-	name, usage string
-	action      func(name string, manager *task.Manager) bool
+	name, description string
+
+	// These are the name of the arguments that are expected by the command.
+	args []string
+
+	// This is the action that runs when this command is invoked. The first parameter to this function
+	// is the name of the command. The function should return true iff the task.Manager should be
+	// persisted to disk after the action returns.
+	action func(name string, manager *task.Manager) bool
+}
+
+// These are the commands used by the anwork application.
+var commands = []command{
+	command{
+		name:        "version",
+		description: "Print version information",
+		args:        []string{},
+		action:      versionAction,
+	},
+	command{
+		name:        "create",
+		description: "Create a new task",
+		args:        []string{"task-name"},
+		action:      createAction,
+	},
+	command{
+		name:        "delete",
+		description: "Delete a task",
+		args:        []string{"task-name"},
+		action:      deleteAction,
+	},
+	command{
+		name:        "show",
+		description: "Show the current tasks",
+		args:        []string{},
+		action:      showAction,
+	},
+	command{
+		name:        "note",
+		description: "Add a note to a task",
+		args:        []string{"task-name", "note"},
+		action:      noteAction,
+	},
+	command{
+		name:        "set-running",
+		description: "Mark a task as running",
+		args:        []string{"task-name"},
+		action:      setStateAction,
+	},
+	command{
+		name:        "set-blocked",
+		description: "Mark a task as blocked",
+		args:        []string{"task-name"},
+		action:      setStateAction,
+	},
+	command{
+		name:        "set-waiting",
+		description: "Mark a task as waiting",
+		args:        []string{"task-name"},
+		action:      setStateAction,
+	},
+	command{
+		name:        "set-finished",
+		description: "Mark a task as finished",
+		args:        []string{"task-name"},
+		action:      setStateAction,
+	},
+	command{
+		name:        "journal",
+		description: "Show the journal",
+		args:        []string{},
+		action:      journalAction,
+	},
+}
+
+// This index starts at "1" since the arg at index "0" will be the command name itself.
+var shiftIndex int = 1
+
+// This function gets the next command line argument (via flag.Args), or panics if there is no such
+// argument.
+func shift() string {
+	if shiftIndex >= flag.NArg() {
+		msg := fmt.Sprintf("Failed to retrieve arg %d", shiftIndex)
+		panic(msg)
+	}
+	val := flag.Arg(shiftIndex)
+	shiftIndex++
+	return val
+}
+
+func versionAction(command string, manager *task.Manager) bool {
+	fmt.Println("ANWORK Version =", version)
+	return false
+}
+
+func createAction(command string, manager *task.Manager) bool {
+	dbgfln("Creating task...")
+	name := shift()
+	manager.Create(name)
+	return true
+}
+
+func showAction(command string, manager *task.Manager) bool {
+	printer := func(state task.State) {
+		fmt.Printf("%s tasks:\n", strings.ToUpper(task.StateNames[state]))
+		for _, task := range manager.Tasks() {
+			if task.State() == state {
+				fmt.Printf("  %s (%d)\n", task.Name(), task.ID())
+			}
+		}
+	}
+	printer(task.StateRunning)
+	printer(task.StateBlocked)
+	printer(task.StateWaiting)
+	printer(task.StateFinished)
+	return false
+}
+
+func noteAction(command string, manager *task.Manager) bool {
+	name := shift()
+	note := shift()
+	manager.Note(name, note)
+	return true
+}
+
+func deleteAction(command string, manager *task.Manager) bool {
+	dbgfln("Deleting task...")
+	name := shift()
+	if !manager.Delete(name) {
+		fmt.Printf("Error! Unknown task: %s\n", name)
+		return false
+	} else {
+		return true
+	}
+}
+
+func setStateAction(command string, manager *task.Manager) bool {
+	name := shift()
+
+	var state task.State
+	command = strings.TrimPrefix(command, "set-")
+	switch command {
+	case "running":
+		state = task.StateRunning
+	case "blocked":
+		state = task.StateBlocked
+	case "waiting":
+		state = task.StateWaiting
+	case "finished":
+		state = task.StateFinished
+	default:
+		panic("Unknown state: " + command)
+	}
+	manager.SetState(name, state)
+	return true
+}
+
+func journalAction(command string, manager *task.Manager) bool {
+	es := manager.Journal().Events
+	for i := len(es) - 1; i >= 0; i-- {
+		e := es[i]
+		fmt.Printf("[%s %s %d %02d:%02d]: %s\n", e.Date.Weekday(), e.Date.Month(), e.Date.Day(),
+			e.Date.Hour(), e.Date.Minute(), e.Title)
+	}
+	return false
 }
