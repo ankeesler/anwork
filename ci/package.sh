b259b212bf6d9bb515e95631f099cbbc7d079d8b
Andrew Keesler <ankeesler1@gmail.com>
2018-01-07 01:00:37 -0500

    Build binaries for both linux/amd64 and darwin/amd64 and use a script to call the right one.

--- ci/package.sh
+++ ci/package.sh
@@ -14,15 +14,18 @@ error() {
     exit 1
 }
 
+build() {
+    exec="anwork_$1_$2"
+    GOOS="$1" GOARCH="$2" go build -o "$exec" github.com/ankeesler/anwork/cmd/anwork
+    if [ $? -ne 0 ]; then
+        error "failed to build anwork executable"
+    fi
+    echo "$exec"
+}
+
 HERE=`basename $PWD`
 if [ "$HERE" != "anwork" ]; then
     error "this script must be run from the root of the anwork directory"
-fi
-
-note "building anwork executable"
-go build github.com/ankeesler/anwork/cmd/anwork
-if [ $? -ne 0 ]; then
-    error "failed to build anwork executable"
 fi
 
 version=$(awk '/const Version =/ {print $NF}' cmd/anwork/command/command.go)
@@ -31,8 +34,9 @@ note "using version $version"
 root="anwork-$version"
 mkdir $root
 mkdir $root/bin
-mv anwork $root/bin
-chmod a+x $root/bin/anwork
+cp ci/anwork $root/bin/
+cp $(build darwin amd64) $root/bin/
+cp $(build linux amd64) $root/bin/
 cp -R doc $root/
 note "created staging directory $root"
 
