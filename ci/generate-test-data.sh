fa373ec655e5736b3cc08f9632b9077bc8df23af
Andrew Keesler <ankeesler1@gmail.com>
2018-08-25 13:29:38 -0400

    Add a script to generate integration test data and do so for v5.

---
+++ ci/generate-test-data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+HERE=`dirname $0`
+
+usage() {
+    echo "usage: `basename $0` <version-number>"
+}
+
+if [[ -z "$1" ]]; then
+    usage
+    exit 1
+fi
+
+version_number=$1
+out_dir=$HERE/../integration/data
+context=v$version_number-context
+
+$HERE/build.sh -h generate-test-data-hash -d generate-test-data-date -o ./anwork
+./anwork -o $out_dir -c $context version
+./anwork -o $out_dir -c $context create task-a
+./anwork -o $out_dir -c $context create task-b
+./anwork -o $out_dir -c $context create task-c
+./anwork -o $out_dir -c $context set-running task-a
+./anwork -o $out_dir -c $context set-blocked task-b
+./anwork -o $out_dir -c $context set-finished task-a
+./anwork -o $out_dir -c $context set-running task-c
+rm anwork
