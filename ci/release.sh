fd779798fcc570961734d3900bb91b663280d7ba
Andrew Keesler <ankeesler1@gmail.com>
2018-09-11 18:42:08 -0400

    Update release script to actually commit stuff.
    This will get the tag to be on the correct sha.

--- ci/release.sh
+++ ci/release.sh
@@ -23,14 +23,23 @@ log "Regenerating CLI documentation..."
 go generate $VERSION_FILE
 log "...done"
 
-log "Adding tag..."
-git tag v$next_version
-log "...done"
-
 log "Updating README with latest version..."
 sed -i .bak -e "s/v$last_version/v$next_version/g" $HERE/../README.md
 log "...done"
 
 log "Updating backwards compat test data..."
 $HERE/generate-test-data.sh $next_version
+log "...done"
+
+log "Running tests..."
+./ci/test.sh
+log "...done"
+
+log "Committing changes..."
+git add -AN
+git commit -m "Release v$next_version."
+log "...done"
+
+log "Adding tag..."
+git tag v$next_version
 log "...done"
