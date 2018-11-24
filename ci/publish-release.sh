c44cd795b1a8b4d00a634117f35633b5517f0405
Andrew Keesler <ankeesler1@gmail.com>
2018-11-24 17:51:46 -0500

    Add release notes into Github release body.

--- ci/publish-release.sh
+++ ci/publish-release.sh
@@ -3,13 +3,14 @@
 set -e
 
 usage() {
-    echo "usage: `basename $0` -t tag -g github-token -a artifact-path"
+    echo "usage: `basename $0` -t tag -g github-token -a artifact-path -d release-doc"
 }
 
 tag=
 token=
 artifact=
-while getopts t:g:a: o
+doc=
+while getopts t:g:a:d: o
 do
     case "$o" in
         t)
@@ -21,33 +22,38 @@ do
         a)
             artifact="$OPTARG"
             ;;
+        d)
+            doc="$OPTARG"
+            ;;
         [?])
             usage
             exit 1
     esac
 done
 
-if [[ -z "$tag" ]] || [[ -z "$token" ]] || [[ -z "$artifact" ]]; then
+if [[ -z "$tag" ]] || [[ -z "$token" ]] || [[ -z "$artifact" ]] || [[ -z "$doc" ]]; then
     usage
     exit 1
 fi
 
 commit="$(git rev-parse $tag)"
+body="$(awk '{printf "%s\\n", $0}' $doc)"
 data=$(cat <<EOF
 {
   "tag_name": "$tag",
   "target_commitish": "$commit",
   "name": "$tag",
+  "body": "$body",
   "draft": true
 }
 EOF
 )
 
 set -x
-upload_url=$(curl -X POST https://api.github.com/repos/ankeesler/anwork/releases \
+response=$(curl -X POST https://api.github.com/repos/ankeesler/anwork/releases \
      -H "Authorization: token $token" \
-     -d "$data" \
-     | jq -r .upload_url)
+     -d "$data")
+upload_url="$(echo $response | jq -r .upload_url)"
 set +x
 
 name="$tag"_anwork_darwin_amd64
