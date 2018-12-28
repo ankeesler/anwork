80837489451c9afec8fe3c9e76702afccbe758c1
Andrew Keesler <ankeesler1@gmail.com>
2018-12-28 14:46:11 -0500

    Regen API documentation on release.

--- ci/release.sh
+++ ci/release.sh
@@ -23,6 +23,10 @@ log "Regenerating CLI documentation..."
 go generate $VERSION_FILE
 log "...done"
 
+log "Regenerating API documentation..."
+go generate ./api/usage.go
+log "...done"
+
 log "Updating README with latest version..."
 sed -i .bak -e "s/v$last_version/v$next_version/g" $HERE/../README.md
 log "...done"
